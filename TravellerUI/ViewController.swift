f7603331d4eb0837713aa2829232b071b6d00946
Abdullah Al Numan <abdullahalnuman235@gmail.com>
2019-12-22 20:02:18 -0800

    Initial Commit

---
+++ TravellerUI/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TravellerUI
+//
+//  Created by Abdullah Al Numan(AAN) on 12/22/19.
+//  Copyright © 2019 Abdullah Al Numan(AAN). All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
